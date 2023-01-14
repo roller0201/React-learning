bc7285fff312e5d7a4f595ca2740d9b341b74eab
roller0201 <119750860+roller0201@users.noreply.github.com>
2023-01-14 19:08:57 +0100

    14.01.23

---
+++ React Samuraj/JS/app.js
@@ -0,0 +1,6 @@
+const fn = (item) => console.log("Podany argument to" + item);
+fn("hej"); // Podany argument to hej
+
+const fn1 = (item) => {
+  return console.log("Podany argument to " + item);
+};
