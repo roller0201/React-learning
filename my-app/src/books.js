a3cda8bbd21f3f5ed394b45442365955a5db72bb
roller0201 <119750860+roller0201@users.noreply.github.com>
2023-01-15 19:30:16 +0100

    15.01.23

---
+++ my-app/src/books.js
@@ -0,0 +1,30 @@
+import img1 from './Images/book1.jpg';
+import img2 from './Images/book2.jpg';
+import img3 from './Images/book3.jpg';
+import img4 from './Images/book4.jpg';
+export const books = [
+  {
+    author: 'Wielu autorów',
+    title: 'MALUSZKOWE MALOWANIE.',
+    img: img1,
+    id: 1,
+  },
+  {
+    author: 'George Orwell',
+    title: 'Folwark zwierzęcy',
+    img: img2,
+    id: 2,
+  },
+  {
+    author: 'Anita Głowińska',
+    title: 'Kicia Kicia',
+    img: img3,
+    id: 3,
+  },
+  {
+    author: 'George Orwell',
+    title: 'Folwark zwierzęcy',
+    img: img4,
+    id: 4,
+  },
+];
