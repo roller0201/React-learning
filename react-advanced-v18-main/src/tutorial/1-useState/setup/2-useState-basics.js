c6f3ba2b0ed14907a07e6f4f670b264cdfa55c19
roller0201 <119750860+roller0201@users.noreply.github.com>
2023-01-17 20:30:41 +0100

    17.01.23

---
+++ react-advanced-v18-main/src/tutorial/1-useState/setup/2-useState-basics.js
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+
+const UseStateBasics = () => {
+  const [text, setText] = useState('random title');
+  const handleClick = () => {
+    if (text === 'random title') {
+      setText('Hello World');
+    } else setText('random title');
+  };
+  return (
+    <>
+      <h1>{text}</h1>
+      <button className='btn' onClick={handleClick}>
+        Change title
+      </button>
+    </>
+  );
+};
+
+export default UseStateBasics;
---
+++ react-advanced-v18-main/src/tutorial/2-useEffect/setup/1-useEffect-basics.js
@@ -0,0 +1,24 @@
+import React, { useState, useEffect } from 'react';
+// by default runs after every re-render
+// cleanup function
+// second parameter
+const UseEffectBasics = () => {
+  const [value, setValue] = useState(0);
+
+  useEffect(() => {
+    if (value >= 1) {
+      document.title = `New Messages(${value})`;
+    }
+  }, [value]);
+
+  return (
+    <>
+      <h1>{value}</h1>
+      <button className='btn' onClick={() => setValue(value + 1)}>
+        Click me
+      </button>
+    </>
+  );
+};
+
+export default UseEffectBasics;
