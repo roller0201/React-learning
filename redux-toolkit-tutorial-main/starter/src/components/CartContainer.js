12f4c79db4bcecbde27cb96ca987134089961973
roller0201 <119750860+roller0201@users.noreply.github.com>
2023-02-05 20:06:52 +0100

    05.02

--- redux-toolkit-tutorial-main/starter/src/components/CartContainer.js
+++ redux-toolkit-tutorial-main/starter/src/components/CartContainer.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import CartItem from './CartItem';
 import { useSelector, useDispatch } from 'react-redux';
-import { clearCart } from '../features/cart/cartSlice';
+
 import { openModal } from '../features/cart/modalSlice';
 
 const CartContainer = () => {
