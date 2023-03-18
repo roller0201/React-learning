671f7d3ce32fc0371847fbfeabfdd64f07f864b7
roller0201 <119750860+roller0201@users.noreply.github.com>
2023-03-18 18:46:19 +0100

    18.03.23

---
+++ Numerology/old/NumerologyOld-main/NumerologyProject/BoundedContext/Numerology.Repository/Mappings/ClientModelMap.cs
@@ -0,0 +1,23 @@
+﻿using FluentNHibernate.Mapping;
+using Numerology.Domain.Models;
+
+namespace Numerology.Repository.Mappings
+{
+    public class ClientModelMap : ClassMap<ClientModel>
+    {
+        public ClientModelMap()
+        {
+            Table("Klienci");
+            Id(e => e.Id).GeneratedBy.Identity();
+            Map(e => e.Name).Column("imie");
+            Map(e => e.Surname).Column("nazwisko");
+            Map(e => e.BirthDate).Column("data_urodzenia");
+            Map(e => e.Active).Column("active");
+            Map(e => e.EntryDate).Column("data_dodania");
+            Map(e => e.Telephone).Column("telefon");
+            Map(e => e.Skype).Column("skype");
+            Map(e => e.Email).Column("email");
+            Map(e => e.Note).Column("notatka");
+        }
+    }
+}
---
+++ Numerology/old/NumerologyOld-main/NumerologyProject/BoundedContext/Numerology.Repository/Mappings/LetterModelMap.cs
@@ -0,0 +1,17 @@
+﻿using FluentNHibernate.Mapping;
+using Numerology.Domain.Models;
+
+namespace Numerology.Repository.Mappings
+{
+    public class LetterModelMap : ClassMap<LetterModel>
+    {
+        public LetterModelMap()
+        {
+            Table("Litery");
+            Id(x => x.Id).GeneratedBy.Identity();
+            Map(x => x.Letter).Column("litera");
+            Map(x => x.Value).Column("wartosc");
+            Map(x => x.Vowel).Column("czy_samogloska");
+        }
+    }
+}
